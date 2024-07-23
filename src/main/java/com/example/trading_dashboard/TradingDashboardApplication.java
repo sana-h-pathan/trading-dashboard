51df46b23ad4d97c3e89d24cad176d2074d40d97
Sana Pathan <sanapathan28@gmail.com>
2024-07-22 20:24:18 -0700

    Initial commit of trading dashboard

---
+++ src/main/java/com/example/trading_dashboard/TradingDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.trading_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trading_dashboard/TradingDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trading_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
